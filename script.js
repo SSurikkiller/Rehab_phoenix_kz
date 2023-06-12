cb04e0340ca113d3545419fde36c2082a73c79a8
SSurikkiller <190107046@stu.sdu.edu.kz>
2023-06-12 12:06:41 +0600

    tryin add some phone numbers

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ function initMap() {
 		zoom: 8,
 	});
 }
+
+
+function Copy(containerid) {
+	  let textarea = document.createElement('textarea');
+	  textarea.id = 'temp';
+	  textarea.style.height = 0;
+	  document.body.appendChild(textarea);
+	  textarea.value = document.getElementById(containerid).innerText;
+	  let selector = document.querySelector('#temp');
+	  selector.select();
+	  document.execCommand('copy');
+	  document.body.removeChild(textarea);
+}
